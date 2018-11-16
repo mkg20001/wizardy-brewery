299606b51db7fa5aebd399c3ffa4c5fef9a508e7
Maciej Krüger <mkg20001@gmail.com>
2018-11-16 14:41:37 +0100

    feat: auto sentry release tracking

--- src/plugins/sentry.js
+++ src/plugins/sentry.js
@@ -8,6 +8,9 @@ module.exports = {
     dsn: Joi.string().required()
   }),
   code: (module, config, env, out) => {
+    if (!config.release && out.version) {
+      config.release = `${module.id}@${out.version}`
+    }
     out.script.push(`const Sentry = require('@sentry/node'); Sentry.init(${JSON.stringify(config)})`)
     out.errScript.push(`const Sentry = require('@sentry/node'); Sentry.captureException(err)`)
   }
